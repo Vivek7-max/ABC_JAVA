7121deec71b4d98fb0a4142b043e01eb0796b3c2
Vivek7-max <nagaralevivek77@gmail.com>
2025-06-21 20:49:45 +0530

    Commit 2

--- CJMAY7012025/src/p6_static/P12.java
+++ CJMAY7012025/src/p6_static/P12.java
@@ -1,10 +1,28 @@
 package p6_static;
-
 public class P12 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int a=test();
+	static int b = 30+print();
+	public static int test() {
+		System.out.println("Test Begin");
+		System.out.println("a="+a);
+		print();
+		System.out.println("b="+b);
+		System.out.println("Test End");
+		return 30;
 	}
-
+	public static void main(String[] args) {
+		System.out.println("Main Begin");
+		System.out.println("a="+a);
+		test();
+		print();
+		System.out.println("b="+b);
+		System.out.println("Main End");
+	}
+	public static int print() {
+		System.out.println("Print Begin");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("Print End");
+		return 60;
+	}
 }
